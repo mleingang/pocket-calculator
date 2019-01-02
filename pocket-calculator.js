219e1e90b692cc41d1ccbea3515d0d1946139f25
First Last <21mleingang@gmail.com>
2019-01-02 12:34:09 -0500

    buttons print numbers

---
+++ pocket-calculator.js
@@ -0,0 +1,71 @@
+var answer = "";
+
+function num1(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "1";
+  output.innerHTML = answer;
+}
+
+function num2(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "2";
+  output.innerHTML = answer;
+}
+
+function num3(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "3";
+  output.innerHTML = answer;
+}
+
+function num4(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "4";
+  output.innerHTML = answer;
+}
+
+function num5(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "5";
+  output.innerHTML = answer;
+}
+
+function num6(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "6";
+  output.innerHTML = answer;
+}
+
+function num7(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "7";
+  output.innerHTML = answer;
+}
+
+function num8(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "8";
+  output.innerHTML = answer;
+}
+
+function num9(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "9";
+  output.innerHTML = answer;
+}
+
+function num0(){
+  let output = document.getElementById("display-bar");
+
+  answer = answer + "0";
+  output.innerHTML = answer;
+}
