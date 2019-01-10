9dd66a59443e2977534f25325e72123a4e202be8
First Last <21mleingang@gmail.com>
2019-01-10 13:24:14 -0500

    will it work? we'll see...

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,106 +1,132 @@
 
-var answer = "";
-
-function num1(){
-  let output = document.getElementById("display");
-
-  answer = answer + "1";
-  output.innerHTML = answer;
-}
-
-function num2(){
-  let output = document.getElementById("display");
-
-  answer = answer + "2";
-  output.innerHTML = answer;
-}
-
-function num3(){
-  let output = document.getElementById("display");
-
-  answer = answer + "3";
-  output.innerHTML = answer;
-}
-
-function num4(){
-  let output = document.getElementById("display");
-
-  answer = answer + "4";
-  output.innerHTML = answer;
-}
-
-function num5(){
-  let output = document.getElementById("display");
-
-  answer = answer + "5";
-  output.innerHTML = answer;
-}
-
-function num6(){
-  let output = document.getElementById("display");
-
-  answer = answer + "6";
-  output.innerHTML = answer;
-}
-
-function num7(){
-  let output = document.getElementById("display");
-
-  answer = answer + "7";
-  output.innerHTML = answer;
-}
-
-function num8(){
-  let output = document.getElementById("display");
-
-  answer = answer + "8";
-  output.innerHTML = answer;
-}
-
-function num9(){
-  let output = document.getElementById("display");
-
-  answer = answer + "9";
-  output.innerHTML = answer;
-}
-
-function num0(){
-  let output = document.getElementById("display");
-
-  answer = answer + "0";
-  output.innerHTML = answer;
-}
+var displayNum = "";
+var storedNum = "";
+var operation = 0;
+var queuedOperation = 0;
+var calculationFinished = false;
 
 function clean(){
-  let output = document.getElementById("display");
+  var display = document.getElementById("display");
 
-  answer = ""
-  output.innerHTML = answer;
+  displayNum = "";
+  storedNum = "";
+  operation = 0;
+  queuedOperation = 0;
+  display.value = displayNum;
 }
 
-function add(){
-  let output = document.getElementById("display");
+function numInput(num){
+  var display = document.getElementById("display");
 
-  answer = answer + "+";
-  output.innerHTML = answer;
+  if ((display.value == "") && num == "0"){
+    return;
+  }else if (calculationFinished == true){
+    display.value = num;
+    calculationFinished = false;
+  }else {
+    display.value += num;
+  }
 }
-
-function sub(){
-  let output = document.getElementById("display");
-
-  answer = answer + "-";
-  output.innerHTML = answer;
-}
-function mult(){
-  let output = document.getElementById("display");
-
-  answer = answer + "x";
-  output.innerHTML = answer;
-}
-
-function divide(){
-  let output = document.getElementById("display");
-
-  answer = answer + "÷";
-  output.innerHTML = answer;
-}
+// function num1(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "1";
+//   output.innerHTML = number;
+// }
+//
+// function num2(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "2";
+//   output.innerHTML = number;
+// }
+//
+// function num3(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "3";
+//   output.innerHTML = number;
+// }
+//
+// function num4(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "4";
+//   output.innerHTML = number;
+// }
+//
+// function num5(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "5";
+//   output.innerHTML = number;
+// }
+//
+// function num6(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "6";
+//   output.innerHTML = number;
+// }
+//
+// function num7(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "7";
+//   output.innerHTML = number;
+// }
+//
+// function num8(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "8";
+//   output.innerHTML = number;
+// }
+//
+// function num9(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "9";
+//   output.innerHTML = number;
+// }
+//
+// function num0(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "0";
+//   output.innerHTML = number;
+// }
+//
+// function clean(){
+//   let output = document.getElementById("display");
+//
+//   number = ""
+//   output.innerHTML = number;
+// }
+//
+// function add(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "+";
+//   output.innerHTML = number;
+// }
+//
+// function sub(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "-";
+//   output.innerHTML = number;
+// }
+// function mult(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "x";
+//   output.innerHTML = number;
+// }
+//
+// function divide(){
+//   let output = document.getElementById("display");
+//
+//   number = number + "÷";
+//   output.innerHTML = number;
+// }
