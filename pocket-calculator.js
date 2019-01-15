e232fb27eb822123441c106edce6a1de9e5e0d70
Mattia Leingang <21mleingang@gmail.com>
2019-01-14 20:16:35 -0500

    uuuuuuuuuuugh

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -9,7 +9,7 @@ function clean(){
   var display = document.getElementById("display");
 
   displayNum = "0";
-  storedNum = "";
+  storedNum = [];
   operation = 0;
   queuedOperation = 0;
   display.innerHTML = displayNum;
@@ -21,24 +21,34 @@ function numInput(num){
   switch(num){
     case 1: storedNum.push("1");
             display.innerHTML += "1";
+            break;
     case 2: storedNum.push("2");
             display.innerHTML += "2";
+            break;
     case 3: storedNum.push("3");
             display.innerHTML += "3";
+            break;
     case 4: storedNum.push("4");
             display.innerHTML += "4";
+            break;
     case 5: storedNum.push("5");
             display.innerHTML += "5";
+            break;
     case 6: storedNum.push("6");
             display.innerHTML += "6";
+            break;
     case 7: storedNum.push("7");
             display.innerHTML += "7";
+            break;
     case 8: storedNum.push("8");
             display.innerHTML += "8";
+            break;
     case 9: storedNum.push("9");
             display.innerHTML += "9";
+            break;
     case 0: storedNum.push("0");
             display.innerHTML += "0";
+            break;
   }
 
 }
