1e08c1e3807138c59b30e36c6f99b6017dd2a31a
First Last <21mleingang@gmail.com>
2019-01-16 13:24:13 -0500

    print first number and operation and second number

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -16,40 +16,38 @@ function clean(){
 function numInput(num){
   var display = document.getElementById("display");
 
-  if (num == )
-
-  // switch(num){
-  //   case 1: storedNum.push("1");
-  //           display.innerHTML += "1";
-  //           break;
-  //   case 2: storedNum.push("2");
-  //           display.innerHTML += "2";
-  //           break;
-  //   case 3: storedNum.push("3");
-  //           display.innerHTML += "3";
-  //           break;
-  //   case 4: storedNum.push("4");
-  //           display.innerHTML += "4";
-  //           break;
-  //   case 5: storedNum.push("5");
-  //           display.innerHTML += "5";
-  //           break;
-  //   case 6: storedNum.push("6");
-  //           display.innerHTML += "6";
-  //           break;
-  //   case 7: storedNum.push("7");
-  //           display.innerHTML += "7";
-  //           break;
-  //   case 8: storedNum.push("8");
-  //           display.innerHTML += "8";
-  //           break;
-  //   case 9: storedNum.push("9");
-  //           display.innerHTML += "9";
-  //           break;
-  //   case 0: storedNum.push("0");
-  //           display.innerHTML += "0";
-  //           break;
-  // }
+  switch(num){
+    case 1: storedNum.push("1");
+            display.innerHTML += "1";
+            break;
+    case 2: storedNum.push("2");
+            display.innerHTML += "2";
+            break;
+    case 3: storedNum.push("3");
+            display.innerHTML += "3";
+            break;
+    case 4: storedNum.push("4");
+            display.innerHTML += "4";
+            break;
+    case 5: storedNum.push("5");
+            display.innerHTML += "5";
+            break;
+    case 6: storedNum.push("6");
+            display.innerHTML += "6";
+            break;
+    case 7: storedNum.push("7");
+            display.innerHTML += "7";
+            break;
+    case 8: storedNum.push("8");
+            display.innerHTML += "8";
+            break;
+    case 9: storedNum.push("9");
+            display.innerHTML += "9";
+            break;
+    case 0: storedNum.push("0");
+            display.innerHTML += "0";
+            break;
+  }
 }
 
 function insertDecimal(dec){
@@ -66,55 +64,43 @@ function insertDecimal(dec){
 function setOperation(command){
   var display = document.getElementById("display");
 
-  if (queuedOperation == 0){
-    storedNum = display.innerHTML;
-  }else if (queuedOperation == 1) {
-    storedNum = evalStored + evalDisplay;
-  }else if (queuedOperation == 2) {
-    storedNum = evalStored - evalDisplay;
-  }else if (queuedOperation == 3) {
-    storedNum = evalStored * evalDisplay;
-  }else if (queuedOperation == 4) {
-    storedNum = evalStored / evalDisplay;
-  }
 
   if (command == "add"){
-    operation = 1;
+    storedNum.push("+");
+    display.innerHTML = "+";
   }else if (command = "subtract") {
-    operation = 2;
+    storedNum.push("-");
+    display.innerHTML = "-";
   }else if (command = "multiply") {
-    operation = 3;
+    storedNum.push("x");
+    display.innerHTML = "x";
   }else if (command = "divide") {
-    operation = 4;
+    storedNum.push("/");
+    display.innerHTML = "/";
   }
-
-  queuedOperation - operation;
-  display.innerHTML = "";
 }
 
-function calculate(){
-  var display = document.getElementById("display");
-        displayNum = display.innerHTML;
-  var evalDisplay = eval(displayNum),
-        evalStored = eval(evalStored);
-
-  if (operation == 1){
-    displayNum = evalStored + evalDisplay;
-  }else if (operation == 2) {
-    displayNum = evalStored - evalDisplay;
-  }else if (operation == 3) {
-    displayNum = evalStored * evalDisplay;
-  }else if (operation == 4) {
-    displayNum = evalStored / evalDisplay;
-  }
-
-  display.innerHTML = displayNum;
-  if (operation != 0){
-    calculationFinished = true;
-  }
-
-  operation = 0;
-  queuedOperation = 0;
-  displayNum = "";
-  storedNum = "";
-}
+// function calculate(){
+//   var display = document.getElementById("display");
+//         displayNum = display.innerHTML;
+//   var evalDisplay = eval(displayNum),
+//         evalStored = eval(evalStored);
+//
+//   if (operation == 1){
+//     displayNum = evalStored + evalDisplay;
+//   }else if (operation == 2) {
+//     displayNum = evalStored - evalDisplay;
+//   }else if (operation == 3) {
+//     displayNum = evalStored * evalDisplay;
+//   }else if (operation == 4) {
+//     displayNum = evalStored / evalDisplay;
+//   }
+  // 
+  // display.innerHTML = displayNum;
+  // if (operation != 0){
+  //   calculationFinished = true;
+  // }
+  //
+  // operation = 0;
+  // displayNum = "";
+  // storedNum = "";
