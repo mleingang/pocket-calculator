e0371ee3b06c2bff44ba7b4c7712a554db964e01
First Last <21mleingang@gmail.com>
2019-01-18 13:22:13 -0500

    wow, it still works!

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -14,49 +14,39 @@ function clean(){
 function numInput(num){
   var display = document.getElementById("display");
 
-  switch(num){
-    case 1: expression.push("1");
-            display.value += "1";
-            break;
-    case 2: expression.push("2");
-            display.value += "2";
-            break;
-    case 3: expression.push("3");
-            display.value += "3";
-            break;
-    case 4: expression.push("4");
-            display.value += "4";
-            break;
-    case 5: expression.push("5");
-            display.value += "5";
-            break;
-    case 6: expression.push("6");
-            display.value += "6";
-            break;
-    case 7: expression.push("7");
-            display.value += "7";
-            break;
-    case 8: expression.push("8");
-            display.value += "8";
-            break;
-    case 9: expression.push("9");
-            display.value += "9";
-            break;
-    case 0: expression.push("0");
-            display.value += "0";
-            break;
+  if (display.value.length > 11)
+    switch(num){
+      case 1: expression.push("1");
+              display.value += "1";
+              break;
+      case 2: expression.push("2");
+              display.value += "2";
+              break;
+      case 3: expression.push("3");
+              display.value += "3";
+              break;
+      case 4: expression.push("4");
+              display.value += "4";
+              break;
+      case 5: expression.push("5");
+              display.value += "5";
+              break;
+      case 6: expression.push("6");
+              display.value += "6";
+              break;
+      case 7: expression.push("7");
+              display.value += "7";
+              break;
+      case 8: expression.push("8");
+              display.value += "8";
+              break;
+      case 9: expression.push("9");
+              display.value += "9";
+              break;
+      case 0: expression.push("0");
+              display.value += "0";
+              break;
   }
-}
-
-function insertDecimal(dec){
-  var display = document.getElementById("display");
-
-  for (i = 0; i < display.value.length; i++){
-    if (display.value.charAt(i) == "."){
-      return;
-    }
-  }
-  display.value += dec;
 }
 
 function setOperation(command){
@@ -75,16 +65,35 @@ function setOperation(command){
   }else if (command == "divide") {
     expression.push("/");
     display.value += "/";
+  }else if (command == "deci") {
+    expression.push(".");
+    display.value += ".";
   }
 }
 
-console.log(expression);
+function percent(){
+  var percNum = [];
 
+  for (var i = (expression.length - 1); i != 0; i--){
+    if((expression[i] == "+") || (expression[i] == "-") || (expression[i] == "x") || (expression[i] == "/")){
+      break;
+    }else {
+      percNum.splice(0, 0, expression[i]);
+    }
+  }
+
+  let joinedPercNum = percNum.join();
+  joinedPercNum.replace(",", " ");
+  for (var i = (joinedPercNum.length - 1); i != 0; i--)
+  console.log(joinedPercNum);
+  joinedPercNum = parseFloat(joinedPercNum) / 100;
+
+  console.log(joinedPercNum);
+}
 
 function calculate(){
   let joinedExpression = expression.join("");
   joinedExpression.replace(",", " ");
   console.log(joinedExpression);
   display.value = eval(joinedExpression);
-
 }
